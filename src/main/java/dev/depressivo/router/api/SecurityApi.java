180e5d91213e242a826023980ab83e339a0332bb
DevDepressivo <dev@depressivo.dev>
2022-12-10 01:11:30 -0300

    Permitindo acesso health

--- src/main/java/dev/depressivo/router/api/SecurityApi.java
+++ src/main/java/dev/depressivo/router/api/SecurityApi.java
@@ -1,6 +1,8 @@
 package dev.depressivo.router.api;
 
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,7 +11,7 @@ import reactor.core.publisher.Mono;
 public class SecurityApi {
 
     @GetMapping("/client")
-    public Mono<RegisteredOAuth2AuthorizedClient> index(RegisteredOAuth2AuthorizedClient client){
+    public Mono<OAuth2AuthorizedClient> index(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
         return Mono.just(client);
     }
 
--- src/main/java/dev/depressivo/router/security/SecurityConfig.java
+++ src/main/java/dev/depressivo/router/security/SecurityConfig.java
@@ -1,23 +1,30 @@
 package dev.depressivo.router.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity httpSecurity){
+        LOG.info("Configurando padroes de autenticacao.");
+
         return httpSecurity
                 .csrf().disable()
                 .cors().disable()
                 .authorizeExchange()
-                .pathMatchers("/about").permitAll()
+                .pathMatchers("/health").permitAll()
                 .anyExchange().authenticated()
                 .and().oauth2Login()
-                .and().formLogin()
                 .and().build();
     }
 
