79663e4d19afd6be785cbe776fc1de02d6de9daa
DevDepressivo <dev@depressivo.dev>
2023-03-07 11:03:40 -0300

    :wrench:Fix buildspec]

--- src/main/java/dev/depressivo/router/api/HealthApi.java
+++ src/main/java/dev/depressivo/router/api/HealthApi.java
@@ -1,5 +1,7 @@
 package dev.depressivo.router.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,11 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/health")
 public class HealthApi {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HealthApi.class);
+
     @GetMapping
     public Mono<String> health(){
+        LOG.info("[HEALTH] Verify Health: OK");
         return Mono.just("OK");
     }
 
